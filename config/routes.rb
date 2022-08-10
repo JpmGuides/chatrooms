fe1389b81d017f56536a6b469abba4e28529c2f7
TGACPNV <theo.gautier@cpnv.ch>
2022-08-10 17:29:50 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  get 'login', to: 'user#show_login'
+  post 'login', to: 'user#login'
+  get 'logout', to: 'user#logout'
+
+  resources :conversation, only: [:show, :create]
+
+  match '*path', to: redirect('/'), via: :all
 end
