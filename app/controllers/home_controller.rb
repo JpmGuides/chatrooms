53cf36da52ee269ea5b1c7ca6f36369303a57c8d
TGACPNV <theo.gautier@cpnv.ch>
2022-08-11 09:30:23 +0200

    refactor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,14 @@
 class HomeController < ActionController::Base
     def index
-        if session[:user_id]
-            user = User.find(session[:user_id])
-            conversations = Conversation.order(:name)
-
-
-            render :index, locals: { user: user, conversations: conversations }
-        else
+        unless session[:user_id]
             redirect_to '/login'
+            return
         end
+        
+        user = User.find(session[:user_id])
+        conversations = Conversation.order(:name)
+
+
+        render :index, locals: { user: user, conversations: conversations }
     end
 end
