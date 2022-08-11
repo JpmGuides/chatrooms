b73b61346be1c01c2e7a14ef4dbe03eea2738a0b
TGACPNV <theo.gautier@cpnv.ch>
2022-08-11 12:09:19 +0200

    show a conversation

--- app/controllers/conversation_controller.rb
+++ app/controllers/conversation_controller.rb
@@ -1,17 +1,26 @@
 class ConversationController < ActionController::Base
+    layout "application"
     def create
         Conversation.create(name: params[:name])
         redirect_to '/'
     end
 
     def show
+        
+
+        # check if user is logged in
+        unless session[:user_id]
+            redirect_to '/login'
+            return
+        end
+
         begin
             conversation = Conversation.find(params[:id])
         rescue ActiveRecord::RecordNotFound
             redirect_back(fallback_location: '/')
-        else
-            render :show, locals: { conversation: conversation }
+            return
         end
-        
+
+        render :show, locals: { conversation: conversation, page_title: conversation.name }
     end
 end
